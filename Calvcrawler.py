56e6ebb76911f11e9391b65567ea78c027de76b0
Calvin Kwan <hkwan003@ucr.edu>
2015-12-17 00:58:39 -0800

    added another file that works

--- Calvcrawler.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import requests
-from bs4 import BeautifulSoup
-
-def trade_spider(max_pages):
-	page = 1
-	while pages < max_pages:
-		url = 'https://www.thenewboston.com/forum/recent_activity.php?page=' + str(page)
